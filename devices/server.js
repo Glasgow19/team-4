2e2ef3bc1a8b5b5b98faa60f9e1f08ee089a0c21
Yan Wu <yanwu2018@gmail.com>
2019-10-26 22:50:49 +0100

    some updates

--- devices/server.js
+++ devices/server.js
@@ -1,16 +1,6 @@
 var express=require('express');
 var app=express();
 
-var mysql      = require('mysql');
-
-var connection = mysql.createConnection({
-    user: 'root',
-    password: '',
-    database: 'test'
-});
-
-connection.connect();
-
 app.get('/',function(req,res)
 {
 res.send('Hello World!');
