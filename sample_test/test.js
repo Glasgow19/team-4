cde6f2a3478452bc1a08591895cafe8612a54afb
Janmayeneric <43655740+Janmayeneric@users.noreply.github.com>
2019-10-27 01:48:44 +0100

    small changes

--- sample_test/test.js
+++ sample_test/test.js
@@ -1,17 +1,1 @@
-<html>
-
-<script>
-document.write(2);   
-var GlasgowTower = {tag:"Glasgow Tower",adjacnet:{ToiletL1},x:631, y:206};
-var ToiletL1 = {tag:"Toilet0a",x:677, y:188}; 
-
-document.write(3);
-function getDistance(objA,objB){
-    return Math.sqrt(Math.pow(objA.x - objB.x,2) + Math.pow(objA.y - objB.y));
-}
-document.write(GlasgowTower.tag);
-
-var json = '[{"name":"Glasgow Tower"},{"name":"The Egg"},{"name":"Workshop area\"},{\"name\":\"Live Lab\"},{\"name\":\"Ticket Desk\"}]";
-
-</script>
-</html>
+groudLiftList = [{'name' : 'Entrance' }, {'name' : 'The Atrium'}, {'name' : 'Ticket Desk' }, {'name' : '' }];
