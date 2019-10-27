8fd4dfe7b3cf73db3fe9a6413e233ade1a2261fd
Yan Wu <yanwu2018@gmail.com>
2019-10-27 06:52:49 +0000

    Simple data model should be done

--- devices/DB_example_populate.sql
+++ devices/DB_example_populate.sql
@@ -1,7 +1,10 @@
 USE Devices_system;
 
-INSERT INTO Devices (Type_of_Devices, Number_of_Available, Number_of_times_borrowed)
-VALUES ('Wheel Chair', 99, 0);
+/*INSERT INTO Devices (Type_of_Devices, Number_of_Available, Number_of_times_borrowed)
+VALUES ('Wheel Chair', 99, 0);*/
 
-INSERT INTO Devices (Type_of_Devices, Number_of_Available, Number_of_times_borrowed)
-VALUES ('Translator', 999, 0);
+INSERT INTO Devices (Type_of_Devices, Number_of_Available, Number_of_times_borrowed, Number_of_borrowing_now)
+VALUES ('Translator', 999, 0, 0);
+
+INSERT INTO Devices (Type_of_Devices, Number_of_Available, Number_of_times_borrowed, Number_of_borrowing_now)
+VALUES ('Wheel Chair', 99, 0, 0);
--- devices/router.js
+++ devices/router.js
@@ -11,7 +11,9 @@ module.exports = (app, connection) => {
     // they attended to the science center
     function find_availability (req, res) {
       let device = req.body.device_borrowed;
-      connection.query('SELECT * FROM Devices WHERE Type_of_Devices = "'+ device+'"', function (err, results, fields) {
+      connection.query('SELECT * FROM Devices WHERE Type_of_Devices = ?',
+      [device],
+      function (err, results, fields) {
         // error will be an Error if one occurred during the query
         // results will contain the results of the query
         // fields will contain information about the returned results fields (if any)
@@ -29,11 +31,13 @@ module.exports = (app, connection) => {
 //The user should be able to book the device online
     function book_device (req, res) {
       let device = req.body.device_borrowed;
-      connection.query('UPDATE Devices SET Number_of_Available = Number_of_Available - 1 , Number_of_times_borrowed = Number_of_times_borrowed + 1 WHERE Type_of_Devices = "'+ device +'"' ,function(err, results, fields){
+      connection.query('UPDATE Devices SET Number_of_Available = Number_of_Available - 1 , Number_of_times_borrowed = Number_of_times_borrowed + 1, Number_of_borrowing_now = Number_of_borrowing_now + 1 WHERE Type_of_Devices = ?',
+      [device],
+      function(err, results, fields){
 
-      if(err) throw err;
+          if(err) throw err;
 
-      console.log(results);
+          console.log(results);
     });}
 
     //The administrater who manage the app is responsible to make sure
@@ -41,7 +45,7 @@ module.exports = (app, connection) => {
     //This method is for user to return the device
     function return_device (req, res) {
       let device = req.body.device_borrowed;
-      connection.query('UPDATE Devices SET Number_of_Available = Number_of_Available + 1 WHERE Type_of_Devices = "'+ device +'"' ,function(err, results, fields){
+      connection.query('UPDATE Devices SET Number_of_Available = Number_of_Available + 1, Number_of_borrowing_now = Number_of_borrowing_now + 1 WHERE Type_of_Devices = "'+ device +'"' ,function(err, results, fields){
 
         if(err) throw err;
 
